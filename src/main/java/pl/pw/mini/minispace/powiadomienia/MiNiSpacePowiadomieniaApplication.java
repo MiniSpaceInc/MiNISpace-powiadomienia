e0b1a736d4af1f76ea3faa4dbff8b7dde83de5c7
Adrian Rudź <01171340@pw.edu.pl>
2024-05-01 13:15:44 +0200

    Initialization of MiNISpace-powiadomienia module

---
+++ src/main/java/pl/pw/mini/minispace/powiadomienia/MiNiSpacePowiadomieniaApplication.java
@@ -0,0 +1,13 @@
+package pl.pw.mini.minispace.powiadomienia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiNiSpacePowiadomieniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiNiSpacePowiadomieniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pw/mini/minispace/powiadomienia/MiNiSpacePowiadomieniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pw.mini.minispace.powiadomienia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiNiSpacePowiadomieniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
