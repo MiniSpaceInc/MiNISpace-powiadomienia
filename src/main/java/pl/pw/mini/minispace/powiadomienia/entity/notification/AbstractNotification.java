89b36dac991c3dc8789fdbcb587cb5300e2793af
Adrian Rudź <01171340@pw.edu.pl>
2024-05-05 11:31:11 +0200

    MS-255 Added uuid to notification

--- src/main/java/pl/pw/mini/minispace/powiadomienia/entity/notification/AbstractNotification.java
+++ src/main/java/pl/pw/mini/minispace/powiadomienia/entity/notification/AbstractNotification.java
@@ -6,10 +6,15 @@ import lombok.Getter;
 import lombok.Setter;
 import pl.pw.mini.minispace.powiadomienia.entity.BaseEntity;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @MappedSuperclass
 public abstract class AbstractNotification extends BaseEntity {
+
+    @Column
+    protected UUID uuid;
 
     @Column
     protected String title;
