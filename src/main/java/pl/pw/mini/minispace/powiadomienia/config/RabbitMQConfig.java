82f30c9ddfb892c51e84ae2adc330a73ad34c405
Adrian Rudź <01171340@pw.edu.pl>
2024-05-19 20:26:58 +0200

    MS-252 - moved RabbitMQ configuration values to configuration file/env variables

--- src/main/java/pl/pw/mini/minispace/powiadomienia/config/RabbitMQConfig.java
+++ src/main/java/pl/pw/mini/minispace/powiadomienia/config/RabbitMQConfig.java
@@ -6,33 +6,68 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    private String NOTIFICATION_EXCHANGE = "notifications";
-    private String NOTIFICATION_CMD_QUEUE = "notification-cmd-queue";
-    private String NOTIFICATION_EVT_QUEUE = "notification-evt-queue";
+    @Value("${spring.rabbitmq.exchange.notification}")
+    private String NOTIFICATION_EXCHANGE;
+    @Value("${spring.rabbitmq.queue.notification.cmd}")
+    private String NOTIFICATION_CMD_QUEUE;
+    @Value("${spring.rabbitmq.queue.notification.evt}")
+    private String NOTIFICATION_EVT_QUEUE;
+    @Value("${spring.rabbitmq.routing-key.notification.cmd}")
+    private String NOTIFICATION_CMD_ROUTING_KEY;
+    @Value("${spring.rabbitmq.routing-key.notification.evt}")
+    private String NOTIFICATION_EVT_ROUTING_KEY;
+    @Value("${spring.rabbitmq.username}")
+    private String RABBIT_USERNAME;
+    @Value("${spring.rabbitmq.password}")
+    private String RABBIT_PASSWORD;
+    @Value("${spring.rabbitmq.host}")
+    private String RABBIT_URL;
+    @Value("${spring.rabbitmq.port}")
+    private Integer RABBIT_PORT;
 
-    private String NOTIFICATION_CMD_ROUTING_KEY = "notification-cmd";
-    private String NOTIFICATION_EVT_ROUTING_KEY = "notification-evt";
-
-    private String RABBIT_USERNAME = "guest";
-    private String RABBIT_PASSWORD = "guest";
-    private String RABBIT_URL = "localhost";
-    private Integer RABBIT_PORT = 5672;
-
+    /**
+     * Creates connection to RabbitMQ Broker
+     * @return ConnectionFactory
+     */
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
         connectionFactory.setUsername(RABBIT_USERNAME);
         connectionFactory.setPassword(RABBIT_PASSWORD);
         connectionFactory.setPort(RABBIT_PORT);
-        connectionFactory.setHost("localhost");
-
+        connectionFactory.setHost(RABBIT_URL);
         return connectionFactory;
+    }
+
+    /**
+     * Sets up rabbitTemplate required to send messages to RabbitMQ Broker
+     * @param connectionFactory
+     * @return RabbitTemplate
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    /**
+     * Sets up message converter to serialize/deserialize messages sent and received from RabbitMQ Broker
+     * @return MessageConverter
+     */
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     /**
