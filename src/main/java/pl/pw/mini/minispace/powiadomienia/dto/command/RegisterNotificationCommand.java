3955d5c11a4606158e109d9b7145273b4ffa7247
Adrian Rudź <01171340@pw.edu.pl>
2024-05-19 20:25:25 +0200

    MS-256 - refactored RegisterNotificationCommand

--- src/main/java/pl/pw/mini/minispace/powiadomienia/dto/command/RegisterNotificationCommand.java
+++ src/main/java/pl/pw/mini/minispace/powiadomienia/dto/command/RegisterNotificationCommand.java
@@ -3,13 +3,17 @@ package pl.pw.mini.minispace.powiadomienia.dto.command;
 import lombok.Getter;
 import lombok.Setter;
 import pl.pw.mini.minispace.powiadomienia.dto.InfoEmailDto;
+import pl.pw.mini.minispace.powiadomienia.dto.NotificationTypeEnum;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Getter
 @Setter
 public class RegisterNotificationCommand implements Serializable {
+    private UUID uuid;
     private String title;
     private String content;
-    private InfoEmailDto emailInfo;
+    private InfoEmailDto infoEmail;
+    private NotificationTypeEnum type;
 }
--- src/main/java/pl/pw/mini/minispace/powiadomienia/mapper/NotificationMapper.java
+++ src/main/java/pl/pw/mini/minispace/powiadomienia/mapper/NotificationMapper.java
@@ -1,7 +1,9 @@
 package pl.pw.mini.minispace.powiadomienia.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.pw.mini.minispace.powiadomienia.dto.NotificationDto;
+import pl.pw.mini.minispace.powiadomienia.dto.command.RegisterNotificationCommand;
 import pl.pw.mini.minispace.powiadomienia.entity.notification.Notification;
 
 @Mapper(componentModel = "spring", uses = {EmailNotificationInfoMapper.class})
@@ -9,5 +11,11 @@ public interface NotificationMapper {
 
     NotificationDto toDto(Notification notification);
 
+    @Mapping(source = "infoEmail", target = "emailInfo")
     Notification fromDto(NotificationDto dto);
+
+    @Mapping(source = "infoEmail", target = "infoEmail")
+    @Mapping(source = "uuid", target = "uuid")
+    @Mapping(source = "type", target = "type")
+    NotificationDto fromRegisterCommand(RegisterNotificationCommand command);
 }
