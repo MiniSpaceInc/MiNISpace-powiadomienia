05b314af24c0a30eabe9e96acabdd0116dfc93b7
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2024-06-09 19:28:56 +0200

    ref

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mvn clean install && java -jar /app/minispace-powiadomienia-1.0.0.jar
+mvn clean install && \
+mvn liquibase:update && \
+mv target/*.jar app.jar && \
+java -jar app.jar
